e06dbb4048084a0812403c5671461b131ccae8c3
Ajay_Kumar <45093975+ajaaykumar@users.noreply.github.com>
2024-06-05 16:51:32 +0000

    understand conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+
+let score = undefined
+// let score = "1abc"
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// converting to Number
+//  "1" => 33
+//  "1abc" => NaN
+//  true => 1; false => 0
+//  ""  => 0; "Ajay" => NaN
+//  null => 0
+//  undefined => NaN
+
+// converting to Boolean
+let isLoggedIn = "Ajay"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let someValue = 33
+let stringConverson = String(someValue)
+console.log(typeof stringConverson);
+console.log(stringConverson);
+
+
+
+
