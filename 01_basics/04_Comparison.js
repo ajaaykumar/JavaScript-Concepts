6b5bf92b7fd41613dcc02aa3a7b1054570e8d925
Ajay_Kumar <45093975+ajaaykumar@users.noreply.github.com>
2024-06-09 10:55:53 +0000

    comparisons

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,12 @@
+// Comparasion
+// Equility check =, ==
+// Stretch check(even check the data type)  ===
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null === 0);
+
+console.log(undefined > 0);
+console.log(null >= 0);
+console.log(null === 0);
+
