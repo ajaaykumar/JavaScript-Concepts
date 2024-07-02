dc0a8ef47cafa29c6e9f22b4119446a2d8358993
Ajay_Kumar <45093975+ajaaykumar@users.noreply.github.com>
2024-07-02 16:13:55 +0000

    objects in detail

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,58 @@
+// const tinderUser = new Object() // singleton 
+const tinderUser = {}
+
+tinderUser.id = 101
+tinderUser.name = 'Ramesh'
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "Ramesh@chatgpt.com",
+    userdetails: {
+        userFullDetails:{
+        firstName:"Ramesh",
+        lastName:'Pakiya',
+        fullname: "Ramesh Pakiya"
+    }
+}
+}
+// accessing objects 
+console.log(regularUser.userdetails.userFullDetails.firstName);
+
+
+// Combining objects
+
+const obj1 = {1:"a",2:"b",3:"c"}
+const obj2 = {4:"a",5:"b",6:"c"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// Insted use spread operater
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+
+
+const users = [
+    {
+        userName:"Ajay"
+    },
+    {
+        userName:"Ramesh"
+    },
+    {
+        userName:"Pakiya"
+    }
+]
+console.log(users[0].userName);
+console.log(Object.keys(regularUser));
+console.log(Object.keys(regularUser.userdetails.userFullDetails));
+console.log(Object.values(regularUser.userdetails.userFullDetails));
+// console.log(Object.entries(regularUser.userdetails.userFullDetails));
+
+// Check if key exists in objects
+console.log(regularUser.hasOwnProperty('email'));
+
+
+
+
+
